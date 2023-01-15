5e29dc33ed485e59d234c931b2ed73a9bc6b2139
unknown <peterlim995@gmail.com>
2023-01-14 20:25:13 -0500

    resposive design

--- public/js/add-comment.js
+++ public/js/add-comment.js
@@ -1,23 +1,24 @@
 const newCommentHandler = async (event) => {
-    event.preventDefault();
-      
-    const comment = document.querySelector('#comment').value.trim();
-    console.log('comment: ',comment);
-  
-    const blog_id = window.location.pathname.split('/').reverse()[0];
+  event.preventDefault();
 
+  const comment = document.querySelector('#comment').value.trim();
+  console.log('comment: ', comment);
+
+  const blog_id = window.location.pathname.split('/').reverse()[0];
+
+  try {
     if (comment) {
       const response = await fetch(`/api/blog/comment`, {
         method: 'POST',
-        body: JSON.stringify({ 
-          blog_id,          
-          comment 
+        body: JSON.stringify({
+          blog_id,
+          comment
         }),
         headers: {
           'Content-Type': 'application/json',
         },
       });
-  
+
       if (response.ok) {
         // alert('response ok');
         document.location.reload();
@@ -25,8 +26,11 @@ const newCommentHandler = async (event) => {
         alert('Failed to create comment');
       }
     }
-  };
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-  document
+document
   .querySelector('.new-comment-form')
   .addEventListener('submit', newCommentHandler);
